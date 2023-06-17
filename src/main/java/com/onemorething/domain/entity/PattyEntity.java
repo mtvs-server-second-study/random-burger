669d72aafc3d8cc0ec483d1f36efb7c5a61b828d
myeonghwan cho <1jmhcho@gmail.com>
2023-06-17 17:47:36 +0900

    파일삭제
    domain.entity에서 필요없는 class 파일 삭제함.

--- src/main/java/com/onemorething/domain/entity/PattyEntity.java
+++
@@ -1,4 +0,0 @@
-package com.onemorething.domain.entity;
-
-public class PattyEntity {
-}
--- src/main/java/com/onemorething/domain/entity/SourceEntity.java
+++
@@ -1,6 +0,0 @@
-package com.onemorething.domain.entity;
-
-public class SourceEntity {
-
-    /* 설명. 번호 */
-}
